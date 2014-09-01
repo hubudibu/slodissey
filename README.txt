a9acb52e0136fe52b5d5e1fa872d1be5a4efee61
Fejes Aniko <oranjje@gmail.com>
2014-09-01 15:10:56 +0200

    initial commit: codepen export

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/hubudibu/pen/kqjHx.
+
+ I know what your #hyperlapse videos need.
---
+++ js/index.js
@@ -0,0 +1,71 @@
+var input = document.getElementById('input');
+var random = document.getElementById('random');
+var aud = document.getElementById('aud');
+var vidcnt = $('.vidcnt');
+var vid = $('#vid');
+
+input.addEventListener('keyup',function(){ 
+  getVidFromURL(input.value);});
+
+random.addEventListener('mouseup',function(){
+  $.ajax({
+    dataType: "jsonp",
+    url:  'https://api.instagram.com/v1/tags/hyperlapse/media/recent?client_id=0395f25a0fd1442cbba6458d7f021217'
+  }).done(function(data){
+    var vid = data.data.filter(function(img){return img.type==='video';})[0];
+   
+           updateShareURL(vid.id); startVid(vid.videos.low_resolution.url);
+  });
+});
+
+
+
+var getVidFromURL = function (url) {
+  $.ajax({
+    dataType: "jsonp",
+    url:  'http://api.instagram.com/oembed?url='+url
+  }).done(function(data){
+    getVidFromID(data.media_id);
+  });
+};
+
+var getVidFromID = function (id) {
+    $.ajax({
+        dataType: "jsonp",
+        url:  'https://api.instagram.com/v1/media/'+id+'?client_id=0395f25a0fd1442cbba6458d7f021217'
+      }).done(function(data2){
+          if (data2.data.videos.low_resolution) {
+             
+           updateShareURL(id); startVid(data2.data.videos.low_resolution.url);
+          } else {
+            vidcnt.removeClass('on');
+          }
+       });
+};
+
+var startVid = function (src) {
+  vid.attr('src', src).get(0).play();
+  vidcnt.addClass('on');
+  aud.play();
+};
+
+
+var URL = 'http://s.codepen.io/hubudibu/full/kqjHx#';
+var share = document.getElementById('sharecnt');
+var updateShareURL = function (id) {
+  share.textContent = URL + id;
+}
+
+share.addEventListener('mouseup',function(){
+  var range = document.createRange();
+  range.selectNode(share);
+  window.getSelection().addRange(range);
+});
+
+try {
+  if (parent.window.location.hash) {
+   getVidFromID(parent.window.location.hash.substr(1)); 
+  }
+} catch (e) {
+  console.log('iframe problems, nvm');
+}
