07cd8f8059366cd17f40fb963a1f2b494e8d9077
Fejes Aniko <oranjje@gmail.com>
2014-09-01 16:12:05 +0200

    share

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+(function () {
+
 var input = document.getElementById('input');
 var random = document.getElementById('random');
 var aud = document.getElementById('aud');
 var vidcnt = $('.vidcnt');
 var vid = $('#vid');
+
+var CLIENTID = '0395f25a0fd1442cbba6458d7f021217';
 
 input.addEventListener('keyup',function(){ 
   getVidFromURL(input.value);});
@@ -10,7 +14,7 @@ input.addEventListener('keyup',function(){
 random.addEventListener('mouseup',function(){
   $.ajax({
     dataType: "jsonp",
-    url:  'https://api.instagram.com/v1/tags/hyperlapse/media/recent?client_id=0395f25a0fd1442cbba6458d7f021217'
+    url:  'https://api.instagram.com/v1/tags/hyperlapse/media/recent?client_id='+CLIENTID
   }).done(function(data){
     var vid = data.data.filter(function(img){return img.type==='video';})[0];
    
@@ -32,7 +36,7 @@ var getVidFromURL = function (url) {
 var getVidFromID = function (id) {
     $.ajax({
         dataType: "jsonp",
-        url:  'https://api.instagram.com/v1/media/'+id+'?client_id=0395f25a0fd1442cbba6458d7f021217'
+        url:  'https://api.instagram.com/v1/media/'+id+'?client_id='+CLIENTID
       }).done(function(data2){
           if (data2.data.videos.low_resolution) {
              
@@ -50,22 +54,29 @@ var startVid = function (src) {
 };
 
 
-var URL = 'http://s.codepen.io/hubudibu/full/kqjHx#';
-var share = document.getElementById('sharecnt');
+// var URL = 'http://s.codepen.io/hubudibu/full/kqjHx#';
+// var share = document.getElementById('sharecnt');
 var updateShareURL = function (id) {
-  share.textContent = URL + id;
+  // share.textContent = URL + id;
+  window.location.href = '#'+id;
+};
+
+// share.addEventListener('mouseup',function(){
+//   var range = document.createRange();
+//   range.selectNode(share);
+//   window.getSelection().addRange(range);
+// });
+
+// try {
+//   if (parent.window.location.hash) {
+//    getVidFromID(parent.window.location.hash.substr(1)); 
+//   }
+// } catch (e) {
+//   console.log('iframe problems, nvm');
+// }
+
+if (window.location.hash) {
+ getVidFromID(window.location.hash.substr(1)); 
 }
 
-share.addEventListener('mouseup',function(){
-  var range = document.createRange();
-  range.selectNode(share);
-  window.getSelection().addRange(range);
-});
-
-try {
-  if (parent.window.location.hash) {
-   getVidFromID(parent.window.location.hash.substr(1)); 
-  }
-} catch (e) {
-  console.log('iframe problems, nvm');
-}
+})();
